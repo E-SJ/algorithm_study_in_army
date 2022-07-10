27043ccb0659e1b041e27ec6223b4d01cb5aae50
E-SJ <>
2022-07-10 13:55:45 +0000

    2. 큰 수의 법칙 complete

--- greedy/2. 큰 수의 법칙.py
+++ greedy/2. 큰 수의 법칙.py
@@ -1,1 +1,12 @@
-print('k')
+n,m,k = map(int, input().split())
+nums = list(map(int,input().split()))
+nums.sort(reverse=True)
+max=nums[0]
+min=nums[1]
+
+full_num=m//(k+1) #반복되는 max*k + min 합의 반복 횟수
+rest_num=m%(k+1) #나머지 합의 반복 횟수
+
+result = (max*k+min)*full_num + max*rest_num
+
+print(result)
