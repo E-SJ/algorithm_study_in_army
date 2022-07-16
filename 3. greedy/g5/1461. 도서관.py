a87b9c761c4f2b333b54a339c68184b1c43d2905
E-SJ <esj7497@gmail.com>
2022-07-16 14:57:15 +0000

    try 1461

---
+++ 3. greedy/g5/1461. 도서관.py
@@ -0,0 +1,70 @@
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
+location = list(map(int,input().split()))
+location.sort(key= lambda x:(abs(x)))
+
+n_location = []
+p_location = []
+for item in location:
+  if (item<0):
+    n_location.append(item)
+  else:
+    p_location.append(item)
+  
+
+
+result=0
+if (location[-1]>0) : #가장 먼거리에 있는 책이 양수위치일때
+  if (len(n_location)%m-1>=0):
+    result+=abs(n_location[len(n_location)%m-1])*2
+  for _ in range(len(n_location)%m):
+    n_location.pop(0)
+    
+  while len(n_location)!=0:  
+    result+=abs(n_location[m-1])*2
+    for _ in range(m):
+      n_location.pop(0)
+
+  if (len(p_location)%m-1>=0 and len(p_location)//m>=1):
+    result+=p_location[len(p_location)%m-1]*2
+  for _ in range(len(p_location)%m and len(p_location)//m>=1):
+    p_location.pop(0)
+
+  while len(p_location)>m:
+    result+=p_location[m-1]*2
+    for _ in range(m):
+      p_location.pop(0)
+      
+  if len(p_location)<=m:
+    result+=p_location[-1]
+
+else:
+  if (len(p_location)%m-1>=0):
+    result+=p_location[len(p_location)%m-1]*2
+  for _ in range(len(p_location)%m-1):
+    p_location.pop(0)
+
+  while len(p_location)!=0:  
+    result+=p_location[m-1]*2
+    for _ in range(m):
+      p_location.pop(0)
+      
+  if (len(n_location)%m-1>=0 and len(n_location)//m>=1):
+    result+=abs(n_location[len(n_location)%m-1])*2
+  for _ in range(len(n_location)%m-1 and len(n_location)//m>=1):
+    n_location.pop(0)
+    
+  while len(n_location)>m:
+    result+=abs(n_location[m-1])*2
+    for _ in range(m):
+      n_location.pop(0)
+  
+  if len(n_location)<=m:
+    result+=abs(n_location[-1])
+
+print(result)
+  
+#아이디어: 먼 거리에 있는 책은 돌아올 필요가 없으므로 먼 거리의 책을 마지막에 두자!
+
+#아이디어를 코드로 푸는 과정이 좀 어렵다. 예제 1번만 오답이 뜨니 그것만 고치자!
