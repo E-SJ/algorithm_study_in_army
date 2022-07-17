c539b64673ec79a9f611a02e158c7de76fdb0a07
E-SJ <esj7497@gmail.com>
2022-07-17 05:59:30 +0000

    complete 1461

--- 3. greedy/g5/1461. 도서관.py
+++ 3. greedy/g5/1461. 도서관.py
@@ -16,51 +16,61 @@ for item in location:
 
 result=0
 if (location[-1]>0) : #가장 먼거리에 있는 책이 양수위치일때
-  if (len(n_location)%m-1>=0):
-    result+=abs(n_location[len(n_location)%m-1])*2
-  for _ in range(len(n_location)%m):
-    n_location.pop(0)
-    
-  while len(n_location)!=0:  
-    result+=abs(n_location[m-1])*2
-    for _ in range(m):
-      n_location.pop(0)
-
-  if (len(p_location)%m-1>=0 and len(p_location)//m>=1):
+  if (len(n_location)>=1):
+    if (len(n_location)<m): # 음수의 책 개수가 들수 있는 것보다 적을 때
+      result+=abs(n_location[-1])*2
+      for _ in range(len(n_location)):
+        n_location.pop(0)
+    else:
+      if (len(n_location)%m>=1 and len(n_location)//m>=1): #음수 - 가까운거 먼저 처리
+        result+=abs(n_location[len(n_location)%m-1])*2
+        for _ in range(len(n_location)%m):
+          n_location.pop(0)
+      
+      while len(n_location)!=0:  #음수 - 모두 처리
+        result+=abs(n_location[m-1])*2
+        for _ in range(m):
+          n_location.pop(0)
+  if (len(p_location)%m>=1 and len(p_location)//m>=1): #양수 - 가까운거 먼저 처리 #(and len(p_location)//m>=1)
     result+=p_location[len(p_location)%m-1]*2
-  for _ in range(len(p_location)%m and len(p_location)//m>=1):
-    p_location.pop(0)
+    for _ in range(len(p_location)%m):
+      p_location.pop(0)
 
-  while len(p_location)>m:
+  while len(p_location)>m: #양수 - 1회를 남기고 모두 처리
     result+=p_location[m-1]*2
     for _ in range(m):
       p_location.pop(0)
-      
-  if len(p_location)<=m:
+
+  if len(p_location)<=m: #양수 - 복귀할 필요 없는 거 처리
     result+=p_location[-1]
 
 else:
-  if (len(p_location)%m-1>=0):
-    result+=p_location[len(p_location)%m-1]*2
-  for _ in range(len(p_location)%m-1):
-    p_location.pop(0)
-
-  while len(p_location)!=0:  
-    result+=p_location[m-1]*2
-    for _ in range(m):
-      p_location.pop(0)
+  if (len(p_location)>=1):
+    if (len(p_location)<m): # 양수의 책 개수가 들수 있는 것보다 적을 때
+      result+=p_location[-1]*2
+      for _ in range(len(p_location)):
+        p_location.pop(0)
+    else:
+      if (len(p_location)%m>=1 and len(p_location)//m>=1): #양수 - 가까운거 먼저 처리
+        result+=p_location[len(p_location)%m-1]*2
+        for _ in range(len(p_location)%m):
+          p_location.pop(0)
       
-  if (len(n_location)%m-1>=0 and len(n_location)//m>=1):
+      while len(p_location)!=0:  #양수 - 모두 처리
+        result+=p_location[m-1]*2
+        for _ in range(m):
+          p_location.pop(0)
+  if (len(n_location)%m>=1 and len(n_location)//m>=1): #음수 - 가까운거 먼저 처리 #(and len(p_location)//m>=1)
     result+=abs(n_location[len(n_location)%m-1])*2
-  for _ in range(len(n_location)%m-1 and len(n_location)//m>=1):
-    n_location.pop(0)
-    
-  while len(n_location)>m:
+    for _ in range(len(n_location)%m):
+      n_location.pop(0)
+
+  while len(n_location)>m: #음수 - 1회를 남기고 모두 처리
     result+=abs(n_location[m-1])*2
     for _ in range(m):
       n_location.pop(0)
-  
-  if len(n_location)<=m:
+
+  if len(n_location)<=m: #음수 - 복귀할 필요 없는 거 처리
     result+=abs(n_location[-1])
 
 print(result)
