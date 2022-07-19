9cd8a7192d3e78ceb42804367c25dcc802924936
E-SJ <esj7497@gmail.com>
2022-07-19 11:10:32 +0000

    complete 1083

--- 3. greedy/g5/1083. sort.py
+++ 3. greedy/g5/1083. sort.py
@@ -3,31 +3,32 @@ elems = list(map(int,input().split()))
 s = int(input())
 count=0
 for i in range(n):
-  if (elems.index(sorted(elems)[-i-1])-i<=s):
-    for j in range(elems.index(sorted(elems)[-i-1])-1,-1+i,-1):
-      temp=elems[j]
-      elems[j]=elems[j+1]
-      elems[j+1]=temp
-      count+=1
-      s-=1
-      print("DEBUG1")
-      if (s<=0):
-        break
+  if (s==0):
+    break
+  #print(f"s={s} len(elems) = {len(elems)}")
+  #print(elems[i:i+min(s+1,len(elems))])
+  #print(elems)
+  
+  maxvalue = max(elems[i:i+min(s+1,len(elems))])
+  if (elems[i]>=maxvalue):
+    continue
   else:
-    print(sorted(elems)[count+s]-1+count,count,-1)
-    for j in range(elems.index(sorted(elems)[count+s])-1+count,count,-1):
-      temp=elems[j]
-      elems[j]=elems[j+1]
-      elems[j+1]=temp
-      s-=1
-      print("DEBUG2")
-      if (s<=0):
+    #print(maxvalue)
+    for j in range(elems.index(maxvalue),i,-1):
+      if (s==0):
         break
-  if (s<=0):
+      temp = elems[j]
+      elems[j] = elems[j-1]
+      elems[j-1]=temp
+      s-=1
+  if (s==0):
     break
 
-for elem in elems:
-  if (elem==elems[-1]):
-    print(elem,end='')
+for i in range(n):
+  if (i==n-1):
+    print(elems[i],end="")
   else:
-    print(elem,end=' ')
+    print(elems[i],end=" ")
+
+#아이디어 : 문제를 너무 복잡하게 생각한 것이 화근이었다.
+#옮길수 있는 s의 값에 따라서 이동시킬 수 있는 최대값을 정하여 맨 앞으로 옮긴다. n<=s-1 라면 당연히 뭐든 맨 앞으로 이동시킬 수 있을 것이고, n>s-1 이라면 s에 값에 따라 인덱스 범위를 조절하고, 만약 s가 남는다면 똑같이 시행하면 s는 곧 0이 될 것이다.
