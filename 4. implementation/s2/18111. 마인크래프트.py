585c96b61472c37f0d6630abc85039d708731daa
E-SJ <esj7497@gmail.com>
2022-07-22 14:09:54 +0000

    try 18111

--- 4. implementation/s2/18111. 마인크래프트.py
+++ 4. implementation/s2/18111. 마인크래프트.py
@@ -6,14 +6,25 @@ mapp=[]
 for _ in range(n):
   mapp.append(list(map(int,input().split())))
 answers=[]
-for height in range(257):
+for height in range(256):
   answer=0
-  blocks=0
+  blocks=b
   for i in range(n):
     for j in range(m):
-      if (height>mapp[i][j]):
-        answer+=(height-mapp[i][j])*1
+      ground=mapp[i][j]
+      if (height>ground):
+        answer+=(height-ground)
         blocks-=1
-      elif (height<mapp[i][j]):
-        answer+=(mapp[i][j]-height)*2
+      elif (height<ground):
+        answer+=(ground-height)*2
         blocks+=1
+      #if (blocks<0):
+      #  break
+    #if (blocks<0):
+    #    break
+  if (blocks>=0):
+    answers.append([answer,height])
+
+answers.sort(key=lambda x:(x[0],x[1]))
+#print(answers)
+print(answers[0][0],answers[0][1],end="")
