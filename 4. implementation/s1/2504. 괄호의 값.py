02f93f1feb145ca38fca6de307b31986e9306e99
E-SJ <esj7497@gmail.com>
2022-07-26 23:36:01 +0900

    try 2504

--- 4. implementation/s1/2504. 괄호의 값.py
+++ 4. implementation/s1/2504. 괄호의 값.py
@@ -1,53 +1,59 @@
 string = list(input())
 small_count=0
 big_count=0
-recent=""
 for i in range(len(string)):
   if string[i]=='(':
     small_count+=1
-    recent='small'
   if string[i]=='[':
     big_count+=1
-    recent='big'
   if string[i]==')':
-    if small_count<1:
-      print('0')
-      exit()
-    else:
-      small_count-=1
+    small_count-=1
   if string[i]==']':
-    if big_count<1:
-      print('0')
-      exit()
-    else:
-      big_count-=1
-  print(big_count,small_count)
+    big_count-=1
 if (small_count!=0) or (big_count!=0):
-  print("debug")
   print('0')
   exit()
 
-cal=[]
 while True:
-  count=0
-  find_flag=False
-  for i in range(len(string)-1):
-    if string[i]+string[i+1]=='()':
-      string[i] = count
-      count+=1
+  i=0
+  while True:
+    if (i>=len(string)-1):
+      break
+    if str(string[i])+str(string[i+1])=='()':
+      string[i] = 2
       del string[i+1]
-      cal.append(2)
-      find_flag=True
-    if string[i]+string[i+1]=='[]':
-      string[i] = count
-      count+=1
+    if (i>=len(string)-1):
+      break
+    if str(string[i])+str(string[i+1])=='[]':
+      string[i] = 3
       del string[i+1]
-      cal.append(3)
-      find_flag=True
-    print(string)
-  if (find_flag==False):
-    break
+    if (i>=len(string)-1):
+      break
+    if ((str(type(string[i]))=="<class 'int'>") and (str(type(string[i+1]))=="<class 'int'>")):
+      string[i]+=string[i+1]
+      del string[i+1]
+    i+=1
+  i=0
+  while True:
+    if (i>=len(string)-2):
+      break
+    if str(string[i])+str(string[i+2])=='()':
+      string[i]=string[i+1]*2
+      del string[i+2]
+      del string[i+1]
+    if (i>=len(string)-2):
+      break
+    if str(string[i])+str(string[i+2])=='[]':
+      string[i]=string[i+1]*3
+      del string[i+2]
+      del string[i+1]
+    i+=1
+  i=0
     
+  if (len(string)==1):
+    break
+
+print(string[0])
 
 
     
