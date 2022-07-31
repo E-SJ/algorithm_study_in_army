a24a5c1eeff1ec90b5890858802615a9db6ffaa3
E-SJ <esj7497@gmail.com>
2022-07-31 09:12:18 +0000

    complete optimization 16926

--- 4. implementation/s1/16926. 배열 돌리기1.py
+++ 4. implementation/s1/16926. 배열 돌리기1.py
@@ -1,13 +1,14 @@
 import sys
 import math
 input=sys.stdin.readline
+from collections import deque
 def print_arr():
   for i in range(n):
     for j in range(m):
       print(arr[i][j],end=" ")
     print()
 
-def revolution(point1,point2):
+def revolution(point1,point2,rev):
   
   """
   for i in range(2*(point2[0]-point1[0]+point2[1]-point1[1])):
@@ -30,24 +31,37 @@ def revolution(point1,point2):
       x-=1
   pos.reverse()
   """
+  queue=deque()
   y=point1[0]
   x=point1[1]
-  
-  
-  temp = arr[y][x]
   for _ in range(point2[1]-point1[1]):
-    arr[y][x] = arr[y][x+1]
+    queue.append(arr[y][x])
     x+=1
   for _ in range(point2[0]-point1[0]):
-    arr[y][x] = arr[y+1][x]
+    queue.append(arr[y][x])
     y+=1
   for _ in range(point2[1]-point1[1]):
-    arr[y][x] = arr[y][x-1]
+    queue.append(arr[y][x])
     x-=1
   for _ in range(point2[0]-point1[0]):
-    arr[y][x] = arr[y-1][x]
+    queue.append(arr[y][x])
     y-=1
-  arr[y+1][x] = temp
+  queue.rotate(-rev)
+  y=point1[0]
+  x=point1[1]
+  for _ in range(point2[1]-point1[1]):
+    arr[y][x] = queue.popleft()
+    x+=1
+  for _ in range(point2[0]-point1[0]):
+    arr[y][x] = queue.popleft()
+    y+=1
+  for _ in range(point2[1]-point1[1]):
+    arr[y][x] = queue.popleft()
+    x-=1
+  for _ in range(point2[0]-point1[0]):
+    arr[y][x] = queue.popleft()
+    y-=1
+  #arr[y+1][x] = temp
     
   
 
@@ -60,8 +74,7 @@ for _ in range(n):
 point1=[0,0]
 point2=[n-1,m-1]
 for _ in range(math.ceil(min(n,m)/2)):
-  for _ in range(r%(2*(point2[0]-point1[0]+point2[1]-point1[1]))):
-    revolution(point1,point2)
+  revolution(point1,point2,r%(2*(point2[0]-point1[0]+point2[1]-point1[1])))
   point1[0]+=1
   point1[1]+=1
   point2[0]-=1
