40dad90bd70d97715e0868abbced793372c9af16
E-SJ <esj7497@gmail.com>
2022-08-11 11:15:47 +0000

    complete 2251

--- 5. DFS, BFS/g5/2251. 물통.py
+++ 5. DFS, BFS/g5/2251. 물통.py
@@ -8,31 +8,52 @@ def bfs():
   while (len(Q)):
     for _ in range(len(Q)):
       item = Q.popleft()
-      print(item)
+      #print(item)
       AB = abs(item[0]-item[1])
       BC = abs(item[1]-item[2])
       AC = abs(item[2]-item[0])
-      if (item[0]==A):
-        if not ([item[0]-AB,item[1]+AB,item[2]] in visited):
-          Q.append([item[0]-AB,item[1]+AB,item[2]])
-          visited.append([item[0]-AB,item[1]+AB,item[2]])
-        if not ([item[0]-AC,item[1],item[2]+AC] in visited):
-          Q.append([item[0]-AC,item[1],item[2]+AC])
-          visited.append([item[0]-AC,item[1],item[2]+AC])
-      elif (item[1]==B):
-        if not ([item[0]+AB,item[1]-AB,item[2]] in visited):
-          Q.append([item[0]+AB,item[1]-AB,item[2]])
-          visited.append([item[0]+AB,item[1]-AB,item[2]])
-        if not ([item[0],item[1]-BC,item[2]+BC] in visited):
-          Q.append([item[0],item[1]-BC,item[2]+BC])
-          visited.append([item[0],item[1]-BC,item[2]+BC])
+      if (item[0]>0 and item[1]<B):
+        water=max(min(item[0],B-item[1]),0)
+        node=[item[0]-water,item[1]+water,item[2]]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
+      if (item[0]>0 and item[2]<C):
+        water=max(min(item[0],C-item[2]),0)    
+        node=[item[0]-water,item[1],item[2]+water]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
+      if (item[1]>0 and item[0]<A):
+        water=max(min(item[1],A-item[0]),0)     
+        node=[item[0]+water,item[1]-water,item[2]]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
+      if (item[1]>0 and item[2]<C):
+        water=max(min(item[1],C-item[2]),0)    
+        node=[item[0],item[1]-water,item[2]+water]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
         
-      elif (item[2]==C):
-        if not ([item[0]+AC,item[1],item[2]-AC] in visited):
-          Q.append([item[0]+AC,item[1],item[2]-AC])
-          visited.append([item[0]+AC,item[1],item[2]-AC])
-        if not ([item[0],item[1]+BC,item[2]-BC] in visited):
-          Q.append([item[0],item[1]+BC,item[2]-BC])
-          visited.append([item[0],item[1]+BC,item[2]-BC])
+      if (item[2]>0 and item[0]<A):
+        water=max(min(item[2],A-item[0]),0)       
+        node=[item[0]+water,item[1],item[2]-water]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
+      if (item[2]>0 and item[1]<B):
+        water=max(min(item[2],B-item[1]),0)
+        node=[item[0],item[1]+water,item[2]-water]
+        if not (node in visited):
+          Q.append(node)
+          visited.append(node)
 bfs()
-print(visited)
+results=[]
+for item in visited:
+  if (item[0]==0 and not item[2] in results):
+    results.append(item[2])
+results.sort()
+for result in results:
+  print(result,end=" ")
