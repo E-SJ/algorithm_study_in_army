e684b8033316f79a2b6e610e59ea756ec3211e34
E-SJ <esj7497@gmail.com>
2022-08-21 09:37:38 +0000

    study extra 13904

--- 6. Sort/g3/13904. 과제.py
+++ 6. Sort/g3/13904. 과제.py
@@ -2,6 +2,36 @@ import sys
 input=sys.stdin.readline
 n = int(input())
 arr=[]
+for _ in range(n):
+  arr.append(tuple(map(int,input().split())))
+arr.sort(key=lambda x:(-x[0],-x[1]))
+#print(arr)
+todo=[0 for _ in range(arr[0][0]+1)]
+for day in range(arr[0][0],0,-1):
+  max=0
+  maxindex=-1
+  for i in range(len(arr)):
+    #print(arr)
+    if (arr[i][0]>=day):
+      if (arr[i][1]>max):
+        max = arr[i][1]
+        maxindex=i
+    else:
+      break
+  todo[day]=max
+  if (maxindex!=-1):
+    arr.pop(maxindex)
+
+#print(todo)
+print(sum(todo))
+  
+
+
+""" 구 코드
+import sys
+input=sys.stdin.readline
+n = int(input())
+arr=[]
 todo=[-1 for _ in range(1001)]
 for _ in range(n):
   arr.append(tuple(map(int,input().split())))
@@ -22,3 +52,4 @@ for d,w in arr:
     count+=1
 
 print(result)
+"""
