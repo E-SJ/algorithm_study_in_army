ee4ead2085a9bc2684bd5aec19eb40c65027db9a
E-SJ <esj7497@gmail.com>
2022-08-21 04:56:28 +0000

    optimize 2473

--- 6. Sort/g3/2473. 세 용액.py
+++ 6. Sort/g3/2473. 세 용액.py
@@ -1,31 +1,24 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-att=list(map(int,input().split()))
-att.sort()
-ans=3000000000
-
-#print_arr(att)
-#print()
+att=sorted([int(x) for x in input().split()])
+ans=float("inf")
 for pivot in range(n-2):
-  point1=pivot+1
-  point2=n-1
-  fix = att[pivot]
+  point1,point2=pivot+1,n-1
   while point1 < point2:
-    #print_arr(sorted([att[point1],att[point2],att[pivot]]))
-    #print()
     p1,p2=att[point1],att[point2]
-    temp = p1+p2+fix
-    if (abs(temp)<abs(ans)):
+    temp = p1+p2+att[pivot]
+    if (abs(temp)<ans):
         ans=abs(temp)
-        left,mid,right=fix,p1,p2
-    if (temp>0):
-      point2-=1
-    elif (temp<0):
+        left,mid,right=att[pivot],p1,p2
+    elif (att[pivot]>=-1):
+      print(left,mid,right)
+      exit()
+    if (temp<0):
       point1+=1
     else:
-      print(fix,p1,p2)
+      point2-=1
+    if ans==0:
+      print(left,mid,right)
       exit()
-
-#print(ans)
 print(left,mid,right)
