69e68123142bb2b51a7bcfa49df60c26dcf956be
E-SJ <esj7497@gmail.com>
2022-09-12 05:57:19 +0000

    complete 11053

--- 8. Dynamic Programming/s2/11053. 가장 긴 증가하는 부분 수열.py
+++ 8. Dynamic Programming/s2/11053. 가장 긴 증가하는 부분 수열.py
@@ -1,11 +1,14 @@
 n = int(input())
-series=list(map(int,input().split()))
-max=-1
-length=0
-for i in range(0,n):
-  if (max<series[i]):
-    max=series[i]
-    length+=1
-print(length)
+arr=list(map(int,input().split()))
+dp=[0 for _ in range(n+1)]
+arr.insert(0,0)
+dp[0]=0
+dp[1]=1
+for i in range(2,n+1):
+  maxindex=0
+  for j in range(1,i):
+    if (arr[j]<arr[i] and dp[maxindex]<dp[j]):
+      maxindex=j
+  dp[i]=dp[maxindex]+1
 
-
+print(max(dp))
