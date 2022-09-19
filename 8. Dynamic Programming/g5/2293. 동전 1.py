939c50ffe6547704153888917effd36b9e1cdee4
E-SJ <esj7497@gmail.com>
2022-09-19 14:14:25 +0000

    study extra 2293

--- 8. Dynamic Programming/g5/2293. 동전 1.py
+++ 8. Dynamic Programming/g5/2293. 동전 1.py
@@ -1,3 +1,4 @@
+#기존 풀이
 import sys
 input=sys.stdin.readline
 n,k = map(int,input().split())
@@ -12,3 +13,24 @@ for value in values:
     if (i-value>=0):
       dp[i]+=dp[i-value]
 print(dp[k])
+"""
+# 2차원 dp 풀이(메모리 초과)
+import sys
+input=sys.stdin.readline
+n,k = map(int,input().split())
+values=[0]
+for _ in range(n):
+  values.append(int(input()))
+values.sort()
+dp=[[0 for _ in range(k+1)] for _ in range(n+1)]
+for i in range(n+1):
+  dp[i][0]=1
+for j in range(1,k+1): #0~i까지 동전 사용
+  for i in range(1,n+1): #j원
+    if (j-values[i]>=0):
+      dp[i][j]=dp[i][j-values[i]]+dp[i-1][j]
+    else:
+      dp[i][j]=dp[i-1][j]
+print(dp)
+"""
+
