3b709875be8dc459e9eea509bcf976edc0ea92d3
E-SJ <esj7497@gmail.com>
2022-09-25 06:10:06 +0000

    study extra 10942

--- 8. Dynamic Programming/g4/10942. 팰린드롬?.py
+++ 8. Dynamic Programming/g4/10942. 팰린드롬?.py
@@ -1,12 +1,9 @@
 import sys
-sys.setrecursionlimit(2000)
 input=sys.stdin.readline
 n = int(input())
 arr = [0]+list(map(int,input().split()))
 m = int(input())
-quest=[]
-for _ in range(m):
-  quest.append(tuple(map(int,input().split())))
+
 dp=[[-1 for _ in range(n+1)] for _ in range(n+1)]
 
 #top-down은 시간 초과. bottom-up 방식으로 재시도해보자.
@@ -26,7 +23,8 @@ for i in range(1,n+1):
     dp[i][j]=returndp(i,j)
 """
 
-#bottom-up
+#bottom-up #방식1
+"""
 for offset in range(0,n):
   for i in range(1,n-offset+1):
     if offset==0:
@@ -35,7 +33,16 @@ for offset in range(0,n):
       dp[i][i+1]=int(arr[i]==arr[i+1])
     else:
       dp[i][i+offset]=min(dp[i+1][i+offset-1],int(arr[i]==arr[i+offset]))
-    
+"""
+#bottom-up #방식2
+for i in range(1,n+1):
+  dp[i][i]=1
+  dp[i-1][i]=arr[i-1]==arr[i]
+  for j in range(i-2,0,-1):
+    dp[j][i]=dp[j+1][i-1]*arr[i]==arr[j]
 
-for s,e in quest:
-  print(dp[s][e])
+
+for _ in range(m):
+  s,e = map(int,input().split())
+  print(int(dp[s][e]))
+  
