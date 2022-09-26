eadac21ae6f677881e216739ca6e0aa87ba35972
E-SJ <esj7497@gmail.com>
2022-09-26 10:54:35 +0000

    complete 1915

--- 8. Dynamic Programming/g4/1915. 가장 큰 정사각형.py
+++ 8. Dynamic Programming/g4/1915. 가장 큰 정사각형.py
@@ -1,24 +1,17 @@
 n,m = map(int,input().split())
 matrix=[[0 for _ in range(m+1)]]
 for _ in range(n):
-  matrix.append([0]+list(map(str,input())))
+  matrix.append([0]+list(map(int,input())))
 dp=[[0 for _ in range(m+1)] for _ in range(n+1)]
 
-
-for step in range(1,max(n,m)+1):
-  for i in range(step,m+1):
-    if step>n or i>m:break
-    if matrix[step][i]=='1':
-      if (matrix[step-1][i-1]==matrix[step][i-1]==matrix[step-1][i]=='1' and dp[step-1][i-1]==dp[step][i-1]==dp[step-1][i]):
-        dp[step][i]=dp[step-1][i-1]+1
-    else:dp[step][i]=max(dp[step-1][i-1],dp[step][i-1],dp[step-1][i],dp[step][i],int(matrix[step][i]))
-  for i in range(step,n+1):
-    if step>m or i>n:break
-    if matrix[i][step]=='1':
-      if (matrix[i-1][step-1]==matrix[i][step-1]==matrix[i-1][step]=='1' and dp[i-1][step-1]==dp[i][step-1]==dp[i-1][step]):
-        dp[i][step]=dp[i-1][step-1]+1
-    else: dp[i][step]=max(dp[i-1][step-1],dp[i][step-1],dp[i-1][step],dp[i][step],int(matrix[i][step]))
-      
+result=0
+for i in range(1,n+1):
+  for j in range(1,m+1):
+    if (i==1 or j==1):
+      dp[i][j]=matrix[i][j]
+    elif matrix[i][j]!=0:
+      dp[i][j]=min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])+1
+    result=max(result,dp[i][j])
 #print(*matrix,sep="\n")
-print(*dp,sep="\n")
-print(dp[n][m]*dp[n][m])
+#print(*dp,sep="\n")
+print(result*result)
