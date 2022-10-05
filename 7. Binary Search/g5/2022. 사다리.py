82e6f3679ccd73e91d320e82a90f97c9e69c6b91
E-SJ <esj7497@gmail.com>
2022-10-05 14:22:54 +0000

    complete 2022

--- 7. Binary Search/g5/2022. 사다리.py
+++ 7. Binary Search/g5/2022. 사다리.py
@@ -1,2 +1,19 @@
-x,y,c = map(int,input().split())
-
+x,y,c = map(float,input().split())
+from math import sqrt
+left=0
+right=3000000000
+while left<=right:
+  mid=(left+right)/2
+  #print("try",mid)
+  if (x**2-mid**2>=0 and y**2-mid**2>=0):
+    temp=(sqrt(x**2-mid**2)*sqrt(y**2-mid**2))/(sqrt(y**2-mid**2)+sqrt(x**2-mid**2))
+    if c-0.0001<temp<c+0.0001:
+      print(mid)
+      exit()
+    elif (temp>c+0.0001):
+      left=mid
+    elif (temp<c-0.0001):
+      right=mid
+  else:
+    right=mid
+  
