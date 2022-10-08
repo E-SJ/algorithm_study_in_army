e428dccc39187933c5c1cf63b31a3619b83f884c
E-SJ <esj7497@gmail.com>
2022-10-08 07:11:45 +0000

    study extra 2467

--- 7. Binary Search/g5/2467. 용액.py
+++ 7. Binary Search/g5/2467. 용액.py
@@ -1,3 +1,30 @@
+#이분 탐색 O(NlogN)
+n=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+#print(arr)
+ans=2000000001
+ans_tuple=tuple()
+for i in range(n-1):
+  left=i+1
+  right=n-1
+  while left<=right:
+    mid=(left+right)//2
+    if (abs(arr[i]+arr[mid])<ans):
+      ans=abs(arr[i]+arr[mid])
+      ans_tuple=(arr[i],arr[mid])
+    if arr[i]+arr[mid]>0:
+      right=mid-1
+    elif arr[i]+arr[mid]<0:
+      left=mid+1
+    else:
+      print(arr[i],arr[mid])
+      exit()
+  
+    
+print(*ans_tuple)
+
+""" 투포인터 O(N)
 n=int(input())
 arr=list(map(int,input().split()))
 arr.sort()
@@ -18,3 +45,4 @@ while p1<p2:
     print(arr[p1],arr[p2])
     exit()
 print(*ans_tuple)
+"""
