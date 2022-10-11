b7aa18b4ca3851ae8984b2584ec54bd04d5282f3
E-SJ <esj7497@gmail.com>
2022-10-11 10:12:14 +0000

    complete 11687

--- 7. Binary Search/s1/11687. 팩토리얼 0의 개수.py
+++ 7. Binary Search/s1/11687. 팩토리얼 0의 개수.py
@@ -1,22 +1,26 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-arr = list(map(int, input().split()))
-left = 0
-right = N-1
-l = 0
-r = 0
-_min = sys.maxsize
-while left < right:
-  _sum = arr[left] + arr[right]
-  if abs(_sum) < _min:
-    l = left
-    r = right
-    _min = abs(_sum)
-  if _sum>0:
-    right-=1
-  elif _sum<0:
-    left+=1
+n = int(input())
+start=0
+end=5000000000
+ans=end
+ansflag=False
+while start<=end:
+  mid=(start+end)//2
+  #print(start,mid,end)
+  count=0
+  mul5=5
+  while mid>=mul5:
+    count+=mid//mul5
+    mul5*=5
+  if (count>n):
+    end=mid-1
+  elif (count<n):
+    start=mid+1
   else:
-    break
-print(arr[l], arr[r])
+    ansflag=True
+    ans=min(ans,mid)
+    end=mid-1
+
+if (not ansflag):
+  print(-1)
+else:
+  print(ans)
