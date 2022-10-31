ca311b28fc19b51951a7787ab46ae57f0e094e78
E-SJ <esj7497@gmail.com>
2022-10-31 06:11:21 +0000

    study extra 9024

--- 7. Binary Search/g5/9024. 두 수의 합.py
+++ 7. Binary Search/g5/9024. 두 수의 합.py
@@ -1,44 +1,39 @@
 import sys
+from bisect import bisect_left, bisect_right
 input=sys.stdin.readline
 t = int(input())
 for T in range(t):
   n,k = map(int,input().split())
   arr = list(map(int,input().split()))
   arr.sort()
+  count=0
+  min_distance=10**9
   for i in range(n):
-    if (arr[i]>k):
-      break
-    else:
-      target=k-arr[i]
+    target=k-arr[i]
+    v1=arr[i]
+    left=i+1
+    right=n-1
+    while left<=right:
+      mid=(left+right)//2
+      v2=arr[mid]
+      if (abs(target-arr[mid])==min_distance):
+        count+=1
+      elif (abs(target-arr[mid])<min_distance):
+        min_distance=abs(target-arr[mid])
+        count=1
+      if arr[mid]>target:
+        right=mid-1
+      elif arr[mid]<target:
+        left=mid+1
+      else:
+        break
+  ans=0
+  #for i in range(n):
+    #ans+=bisect_right(arr[i+1:],k-arr[i]-min_distance)-bisect_left(arr[i+1:],k-arr[i]-min_distance)
+    #ans+=bisect_right(arr[i+1:],k-arr[i]+min_distance)-bisect_left(arr[i+1:],k-arr[i]+min_distance)
+  print(count)
       
-      left=i+1
-      right=n-1
-      while left<right:
-        mid=(left+right)//2
-        if mid>target:
-          right=mid-1
-        elif mid<target:
-          left=mid+1
-        else:
-          while mid-1>i and arr[mid]==arr[mid-1]:
-            mid-=1
-          break
-      start_index=mid
-      
-      left=i+1
-      right=n-1
-      while left<right:
-        mid=(left+right)//2
-        if mid>target:
-          right=mid-1
-        elif mid<target:
-          left=mid+1
-        else:
-          while mid+1<n and arr[mid]==arr[mid+1]:
-            mid+=1
-          break
-      end_index=mid
-    print("result",T,i,start_index,end_index,target,arr[start_index],arr[end_index])
+    #print("result",T,i,start_index,end_index,target,arr[start_index],arr[end_index])
 
       
       
