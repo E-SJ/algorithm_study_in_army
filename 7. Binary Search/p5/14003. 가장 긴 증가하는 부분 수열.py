83b3a3969b79ea76ad7f81ac7727437538c228df
E-SJ <esj7497@gmail.com>
2022-11-04 11:14:42 +0000

    complete 14003

--- 7. Binary Search/p5/14003. 가장 긴 증가하는 부분 수열.py
+++ 7. Binary Search/p5/14003. 가장 긴 증가하는 부분 수열.py
@@ -4,17 +4,28 @@ n = int(input())
 arr = list(map(int,input().split()))
 dp=[0]*(n+1)
 lis=[arr[0]]
+rec=[0]
 #len_lis=1
 for i in range(1,n):
   if lis[-1]<arr[i]:
     lis.append(arr[i])
+    rec.append(len(lis)-1)
   else:
     index=bisect_left(lis,arr[i])
     lis[index]=arr[i]
+    rec.append(index)
   #print(*lis)
 
-
+temp=max(rec)
+ans=[]
+for i in range(n-1,-1,-1):
+  if rec[i]==temp:
+    ans.append(arr[i])
+    temp-=1
+#print(lis)
+#print(rec)
 print(len(lis))
+print(*ans[::-1])
 #print(*lis)
 """
 print(max(dp))
